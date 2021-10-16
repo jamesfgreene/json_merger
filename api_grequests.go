a31d85c4344d41b79fab0f89155b53cd5a124594
jamesfgreene <jamesfgreene@gmail.com>
2021-10-15 17:07:08 -0700

    Update api_grequests.go

--- api_grequests.go
+++ api_grequests.go
@@ -4,3 +4,90 @@ resp, err := grequests.post("http://api.jimmy.com/pants?type=skinnyjeans",
 				Headers: map[string]string{"X-Riscosity-Tkn": token},
 				IsAjax: true, // this adds the X-Requested-With: XMLHttpRequest header
 			})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+resp, err := grequests.post("http://api.jimmy.com/pants?type=skinnyjeans",
+			&grequests.RequestOptions{
+        JSON:   map[string]string{"token": "mah token", "boo":"baz", "bop":{"foo":{"bat":"boo"}}},
+				Headers: map[string]string{"X-Riscosity-Tkn": token},
+				IsAjax: true, // this adds the X-Requested-With: XMLHttpRequest header
+			})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+resp, err := grequests.patch("http://api.jimmy.com/pants?type=skinnyjeanspatch",
+			&grequests.RequestOptions{
+        JSON:   map[string]string{"token": "mah token", "boo":"baz", "bop":{"foo":{"bat":"boo"}}},
+				Headers: map[string]string{"X-Riscosity-Tkn": token},
+				IsAjax: true, // this adds the X-Requested-With: XMLHttpRequest header
+			})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+resp, err := grequests.put("http://api.jimmy.com/pants?type=skinnyjeansput",
+			&grequests.RequestOptions{
+        JSON:   map[string]string{"token": "mah token", "boo":"baz", "bop":{"foo":{"bat":"boo"}}},
+				Headers: map[string]string{"X-Riscosity-Tkn": token},
+				IsAjax: true, // this adds the X-Requested-With: XMLHttpRequest header
+			})
